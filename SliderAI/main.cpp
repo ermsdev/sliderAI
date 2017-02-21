97e0c5df587f14020a0d6addb4c2ca21d301045d
Stephen <stephen@stephens-mbp.wireless.skynet.local>
2017-02-20 21:24:19 -0800

    initial command

---
+++ SliderAI/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  SliderAI
+//
+//  Created by Stephen on 2/20/17.
+//  Copyright © 2017 Stephen. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
