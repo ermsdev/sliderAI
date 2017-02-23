e7ecd843247721bf62d81493787d85bcddc00023
Stephen <stephen.ermshar@wallawalla.edu>
2017-02-23 13:56:50 -0800

    changed board display and method for moving tiles

--- SliderAI/main.cpp
+++ SliderAI/main.cpp
@@ -9,11 +9,11 @@ int main( ) {
     
     int rootLayout[9] = {1,0,2,4,6,3,7,5,8}; //solvable in <10 moves, index = boardPosition
     
-    board tb1(rootLayout);
+    board tb1(rootLayout, 1);
     
     tb1.coutLayout();
     
-    board tb2(tb1, 0, 2);
+    board tb2(tb1);
     
     tb2.coutLayout();
     
--- SliderAI/sliderai.h
+++ SliderAI/sliderai.h
@@ -13,13 +13,14 @@ using namespace std;
 
 class board {
 private:
-    int layout[3];
+    int layout[9];
+    int blankLocation; // so we don't have to search through board for the blank tile
 public:
-    //inline functions to be moved to implementation when working
-    board(int *n);
-    board(board &b, int pos1, int pos2);
+    board(int *n, int bL);
+    board(board &b /*, int move*/);
     int* getLayout(){return layout;}
     void coutLayout();
+    //char* listMoves();
 };
 
 #endif /* sliderai_h */
